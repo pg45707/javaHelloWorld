8b684227d6f48ea5f129a426cb24fab0e6fc8742
Prabhu <pgarnayak@gmail.com>
2018-10-04 15:25:14 +0530

    [3] Jenkins ready project

---
+++ JenkinsTest.java
@@ -0,0 +1,11 @@
+
+public class JenkinsTest {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello Jenkins !!!");
+		System.out.println("SUCESSFULLY IMPLEMENTED AND DEPLOYED JENKINS WITH GIT");
+
+	}
+
+}
